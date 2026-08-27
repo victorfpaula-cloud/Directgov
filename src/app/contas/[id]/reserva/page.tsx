a4da14f7d8c0b69ef65880f78f0de71db51a6c15
victorfpaula-cloud <victorfpaula@gmail.com>
2026-08-27 16:33:31 -0300

    Update page.tsx

--- src/app/contas/[id]/reserva/page.tsx
+++ src/app/contas/[id]/reserva/page.tsx
@@ -1,6 +1,8 @@
 import { criarClienteAdmin } from "@/lib/supabase/admin";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const fetchCache = "force-no-store";
 
 export default async function ReservaConfigPage({
   params,
