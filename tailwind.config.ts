727c3cc11037e7cd0922269110cd7c834ecdc0a7
victorfpaula-cloud <victorfpaula@gmail.com>
2026-08-27 22:58:51 -0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,9 +12,14 @@ const config: Config = {
           "0%": { opacity: "0", transform: "scale(0.85)" },
           "100%": { opacity: "1", transform: "scale(1)" },
         },
+        "cd-barra": {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(300%)" },
+        },
       },
       animation: {
         "pop-in": "pop-in 0.45s ease-out",
+        "cd-barra": "cd-barra 1s ease-in-out infinite",
       },
     },
   },
